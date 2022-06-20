b37db2204b2d48d62d397bf8c4f81bced6911a96
Thejasvi Velaga <51443806+thejasvi204@users.noreply.github.com>
2022-06-20 16:47:33 -0400

    Add files via upload

---
+++ pre_course1/pre_1.py
@@ -0,0 +1,35 @@
+# Time Complexity :
+# Space Complexity :
+# Did this code successfully run on Leetcode :
+# Any problem you faced while coding this :
+class myStack:
+    # Please read sample.java file before starting.
+    # Kindly include Time and Space complexity at top of each file
+    def __init__(self):
+        self.arr=[]
+    def isEmpty(self):
+        if self.arr:
+            return 0
+        else:
+            return 1
+    def push(self, item):
+        self.arr.append(item)
+    def pop(self):
+        if self.isEmpty() == 0:
+            self.arr.pop()
+        else:
+            print("stack is empty")
+    def peek(self):
+        return self.arr[-1]
+    def size(self):
+        return len(self.arr)
+    def show(self):
+        print(self.arr)
+
+s = myStack()
+s.push('1')
+s.push('2')
+s.push('3')
+print(s.pop())
+print(s.pop())
+print(s.show())
---
+++ pre_course1/pre_2.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    def __init__(self):
+        self.head=None
+        self.node=None
+    def push(self, data):
+        if self.head== None:
+            self.head=Node(data)
+        else:
+            self.node=Node(data)
+            self.node.next=self.head
+            self.head=self.node
+
+    def pop(self):
+        if self.head==None:
+            print("stack is empty")
+        else:
+            pop=self.head.data
+            self.head=self.head.next
+            return pop
+
+a_stack = Stack()
+while True:
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    print('push <value>')
+    print('pop')
+    print('quit')
+    do = input('What would you like to do? ').split()
+    # Give input as string if getting an EOF error. Give input like "push 10" or "pop"
+    operation = do[0].strip().lower()
+    if operation == 'push':
+        a_stack.push(int(do[1]))
+    elif operation == 'pop':
+        popped = a_stack.pop()
+        if popped is None:
+            print('Stack is empty.')
+        else:
+            print('Popped value: ', int(popped))
+    elif operation == 'quit':
+        break
